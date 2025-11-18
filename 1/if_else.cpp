c1c9d6af80f853b806fd662259071fd2c67f00c3
iamrazzakk <rakibt23p@gmail.com>
2025-11-18 20:40:46 +0600

    learn if else and ternary operator

---
+++ 1/if_else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 11;
+    if (x % 2 == 0)
+    {
+        /* code */
+        cout << "Even" << endl;
+    }
+    else
+    {
+        cout << "odd" << endl;
+    }
+
+    return 0;
+}
---
+++ 1/ternary_operator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
+    x % 2 == 0 ? cout << "Even" << endl : cout << "Odd" << endl;
+
+    return 0;
+}
