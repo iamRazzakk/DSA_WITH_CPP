eeaf4215f260abe8a2d5a7999bde9a66234429e6
iamrazzakk <rakibt23p@gmail.com>
2025-12-21 15:46:25 +0600

    resolve the problem range sum query with base on time complexity

---
+++ Y_Range_sum_query.cpp
@@ -0,0 +1,45 @@
+/**
+ * Author : Razzak
+ * Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Y
+ */
+
+ #include <bits/stdc++.h>
+ using namespace std;
+ 
+ int main()
+ {
+     int n, q;
+     cin >> n >> q;
+ 
+     vector<long long> v(n);
+     for (int i = 0; i < n; i++)
+     {
+         cin >> v[i];
+     }
+ 
+     vector<long long> pre(n);
+     pre[0] = v[0];
+     for (int i = 1; i < n; i++)
+     {
+         pre[i] = pre[i - 1] + v[i];
+     }
+ 
+     while (q--)
+     {
+         int l, r;
+         cin >> l >> r;
+ 
+         l--; r--; // convert to 0-based
+ 
+         long long sum;
+         if (l == 0)
+             sum = pre[r];
+         else
+             sum = pre[r] - pre[l - 1];
+ 
+         cout << sum << endl;
+     }
+ 
+     return 0;
+ }
+ 
