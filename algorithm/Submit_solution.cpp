85e4493d40484320a62f3b8bb2d8dc66440a7e6a
iamrazzakk <rakibt23p@gmail.com>
2025-12-27 11:55:34 +0600

    resolve submit solution

---
+++ algorithm/Submit_solution.cpp
@@ -0,0 +1,20 @@
+// Problem: https://codeforces.com/group/MWSDmqGsZm/contest/219158/submit
+// Author : abdur_Razzak_rakib
+// Date   : 2025-12-26
+// =======================================
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+    long long int n;
+    cin >> n;
+    long long int sum = n * (n + 1) / 2;
+    cout << sum << endl;
+    return 0;
+
+}
