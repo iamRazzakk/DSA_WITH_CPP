7e4efd0d0806b96317a13c737c79b0fd6e9d1d9c
iamrazzakk <rakibt23p@gmail.com>
2026-01-17 09:56:54 +0600

    add implementation for reversing data in a linked list using recursion

---
+++ linked_list/reverse_data_using_recursion.cpp
@@ -0,0 +1,58 @@
+// =======================================
+// Problem: reverse data using recursion
+// Author : abdur_Razzak_rakib
+// Date   : 2026-01-17
+// =======================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node {
+    public: int val;
+    Node * next;
+    Node(int val) {
+        this -> val = val;
+        this -> next = NULL;
+    }
+
+};
+
+
+void insurt_at_tail(Node* & head, Node* & tail, int val){
+    Node* newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+
+void print_reverse_using_recursion(Node* temp){
+    if(temp == NULL){
+        return;
+    }
+    print_reverse_using_recursion(temp->next);
+    std::cout << temp->val << std::endl;
+}
+
+
+int main() {
+
+    Node* head = NULL;
+    Node* tail = NULL;
+    
+    
+    while(true){
+        int val; 
+        std::cin >> val;
+        if(val == -1){
+            break;
+        }
+        insurt_at_tail(head, tail, val);
+    }
+    print_reverse_using_recursion(head);
+}
