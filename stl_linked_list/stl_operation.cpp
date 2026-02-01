9bee83f98248a97b7ac9e27a44322f452fa2ff55
iamrazzakk <rakibt23p@gmail.com>
2026-02-01 20:27:16 +0600

    reverse value and return unique value for that

--- stl_linked_list/stl_operation.cpp
+++ stl_linked_list/stl_operation.cpp
@@ -6,19 +6,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    list <int> l ={10,20,30,40,50,60,10,20,10};
+int main()
+{
+    list<int> l = {10, 20, 30, 40, 50, 60, 10, 20, 10};
     // ! for remove specipice value
     // l.remove(10);
 
-
     // ! for sort the linkelist
-    l.sort();
+    // l.sort();
+
+    // ! sort value dec order
+
+    // l.sort(greater<int>());
+    // ! for remove duplicate value but for that i need to do sort the linked list
+    // l.unique();
+
+
+    // for reverse the linked list
+
+    l.reverse();
     for (int list : l)
     {
         /* code */
         cout << list << endl;
     }
-    
+
     return 0;
 }
